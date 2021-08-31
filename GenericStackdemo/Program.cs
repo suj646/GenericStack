fa563653390cd25fee1e56ab3af7fbde03cff402
Sujit Bagade <sujitbagade2@gmail.com>
2021-08-31 22:56:25 +0530

    GenricStack

---
+++ GenericStackdemo/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GenericStackdemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Stack<string> mystack = new Stack<string>();
+            mystack.Push("Sunday");
+            mystack.Push("Monday");
+            mystack.Push("Tuesday");
+            mystack.Push("Wednesday");
+            mystack.Push("Thursday");
+            mystack.Push("Friday");
+            mystack.Push("Saturday");
+            print(mystack);
+            Console.WriteLine("\nTop Item is : {0}", mystack.Peek());
+            Console.WriteLine("\nRemoved Top Item of Stack : " + mystack.Pop());
+            Console.WriteLine("\nNow Stack's Items are : ");
+            print(mystack);
+        }
+        public static void print(Stack<string> sp)
+        {
+            foreach (string s in sp)
+            {
+                Console.Write(s.ToString() + " ");
+            }
+        }
+    }
+}
+        
